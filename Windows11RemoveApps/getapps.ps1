5d0e5a712ee6807cea08945c09e8144c693b90a5
qewade0701 <62809155+qewade0701@users.noreply.github.com>
2024-01-03 14:54:03 -0600

    Add files via upload

---
+++ Windows11RemoveApps/getapps.ps1
@@ -0,0 +1,1 @@
+﻿$Filename = "C:\Temp\Appx.txt"$Appx = Get-AppxPackage | select name$Appx | Out-File -FilePath $Filename$Apps = Get-Content $Filename$Apps | foreach{ $_.Trim()} | Out-File $Filename
